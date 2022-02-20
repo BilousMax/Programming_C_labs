dcce3404990f8ecbc3462a53e724e7508b900024
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-02-20 20:10:28 +0200

    lab0101

---
+++ lab01/lab0101/lab0101.cpp
@@ -0,0 +1,16 @@
+﻿//Дано два числа. Знайти середнє арифметичне їх квадратів і середнє арифметичне їх модулів.
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	float a, b;
+	cin >> a;
+	cin >> b;
+	cout << (pow(a, 2) + pow(b, 2)) / 2 << endl;
+	cout << (abs(a) + abs(b)) / 2 << endl;
+	return 0;
+}
+
