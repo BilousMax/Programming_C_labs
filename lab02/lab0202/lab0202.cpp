958d318b169e6bca4fed03942f94b2607871a180
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-04-10 18:34:31 +0300

    lab0202

---
+++ lab02/lab0202/lab0202.cpp
@@ -0,0 +1,18 @@
+﻿
+// 7
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	double x, a = 0, b = 0, c = 0;
+	cin >> x;
+	a = tan(x);
+	b = abs(x);
+	c = pow(1 + x * x, 4);
+	cout << a << "\n";
+	cout << b << "\n";
+	cout << c << "\n";
+}
