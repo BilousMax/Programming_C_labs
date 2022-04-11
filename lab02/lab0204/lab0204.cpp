e79b3e9cec4079b183f9e828a840d774da115fac
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-04-11 09:08:32 +0300

    lab0204

---
+++ lab02/lab0204/lab0204.cpp
@@ -0,0 +1,30 @@
+﻿// варіант 4
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+    float F, a, b, c, x = 0;
+    cout << "Введiть число a: \n";
+        cin >> a;
+        cout << "Введiть число b: \n";
+        cin >> b;
+        cout << "Введiть число c: \n";
+        cin >> c;
+        cout << "Введiть число x: \n";
+        cin >> x;
+        if ((x < 0) && (b!=0)) {
+            F = (-a) * x - c;
+        }
+        else if ((x > 0) && (b == 0)) {
+            F = (x - a) / c;
+        }
+        else {
+            F = (b * x) / (c - a);
+        }
+    cout << F;
+}
+
