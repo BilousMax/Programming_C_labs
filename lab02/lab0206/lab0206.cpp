480a1c9cbc2b27e3f7e5912d1886340d6973d7e3
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-04-17 20:49:36 +0300

    lab0206

---
+++ lab02/lab0206/lab0206.cpp
@@ -0,0 +1,21 @@
+﻿// варіант 10
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "ukr");
+	double x = 0;
+	int i, n;
+	cout << "Введiть n\n";
+	cin >> n;
+	for ( i = 1; i <= n; i++)
+	{
+		x = (sin(1) * sin(1 + 2) * sin(1 + 2 + n));
+	}
+	cout << x;
+}
+
+
