c32c3493fa83fbdedb5cd410e05015317aac02e4
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-04-17 22:47:57 +0300

    lab0208

---
+++ lab02/lab0208/lab0208.cpp
@@ -0,0 +1,23 @@
+﻿// 3
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    long long int k, sum = 0;
+    double q;
+    cout << "k\n";
+    cin >> k;
+    for (int  q= 1; q < k; q++)
+    {
+        if (abs(q) < pow(k,2))
+        {
+            sum=sum+q;
+        }
+    }
+    cout << sum;
+}
+
+
