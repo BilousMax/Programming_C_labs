baa1f675139155f9feb70337b6a058f8e0e52273
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-04-26 22:46:41 +0300

    lab0302

---
+++ lab03/lab0302/lab0302.cpp
@@ -0,0 +1,22 @@
+﻿// 3
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "ukr");
+	long double a, b;
+	cout << "Введiть a:\n";
+	cin >> a;
+	cout << "Введiть b:\n";
+	cin >> b;
+	long double* pa = &a;
+	long double* pb = &b;
+	cout << "Середнє арифметичне число\n";
+	cout << (*pa + *pb) / 2 << endl;
+	cout << "Середнє геометричне\n";
+	cout << sqrt(*pa * *pb) << endl;
+
+}
+
