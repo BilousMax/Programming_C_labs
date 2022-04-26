c1063025bccac4df5a259319928b6d5258aaabe7
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-04-26 23:10:29 +0300

    lab0303

---
+++ lab03/lab0303/lab0303.cpp
@@ -0,0 +1,21 @@
+﻿// 2
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	new float;
+	float *ptr = new float;
+	cin >> *ptr;
+
+	new float;
+	float *otherptr = new float;
+	cin >> *otherptr;
+
+	*ptr = *ptr * 2;
+	cout << "*ptr=" << *ptr;
+
+}
+
+
