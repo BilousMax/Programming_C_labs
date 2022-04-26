cba519fd0ca9f1c3dd08c26597082fc1cf8e5958
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-04-26 21:51:25 +0300

    lab0301

---
+++ lab03/lab0301/lab0301.cpp
@@ -0,0 +1,25 @@
+﻿// 5 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int temp, a = 0, b = 0;
+    cout << "Enter a: \n";
+    cin >> a;
+    cout << "Enter b: \n";
+    cin >> b;
+    int *pa = &a;
+    int *pb = &b;
+    *pa = *pa * 2;
+    temp = *pa;
+    *pa = *pb;
+    *pb = temp;
+    cout << "RESULT\n";
+    cout << "a=";
+    cout << a << endl;
+    cout << "b=";
+    cout << b << endl;
+    
+}
+
