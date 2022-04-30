d0f07cd2f4930c82c9e15c6f717ab81b2e89b396
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-05-01 00:25:22 +0300

    lab0402

--- lab04/lab0402/lab0402.cpp
+++ lab04/lab0402/lab0402.cpp
@@ -2,28 +2,34 @@
 
 #include <iostream>
 using namespace std;
-main()
+int main()
 {
     int a[100], b[100];
     int i, max;
-    for (i = 0; i < 100; i++)
-        ;
-    max = a[0];
+    srand((unsigned)time(NULL));
+    for (int i = 1; i < 100; i++)
+    {
+        a[i] = 100. - ((double)rand() / RAND_MAX) * 200.;
+        max = a[0];
+    }
+    
     for (i = 0; i < 100; i++)
     {
-        if (a[i] > max)
-            max = a[i];
+        max = a[i];
     }
+    cout << max<<"\n"; 
     for (i = 0; i < 100; i++)
     {
         if (a[i] != max)
             b[i] = 0;
         else
             b[i] = 1;
-        cout << b[i];
     }
-    return 0;
+    cout<<b[i]; // не можу зрозуміти чому виводиться число -858993460
+    
 }
+
+
 
 
 
