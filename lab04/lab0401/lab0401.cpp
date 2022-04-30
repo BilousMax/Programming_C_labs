7873a9c7b75b89a6862e8a29b5ca3d76dcef02e6
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-04-30 23:55:56 +0300

    lab0401

---
+++ lab04/lab0401/lab0401.cpp
@@ -0,0 +1,31 @@
+﻿// 13
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ukr");
+    int a[30], b[30];
+    srand((unsigned)time(NULL));
+    for (int i = 1; i <= 30; i++)
+    {
+        a[i] = 100. - ((double)rand() / RAND_MAX) * 200.; 
+    }
+        
+        for (int i = 1; i <= 30; i++)
+        {
+            b[i] = 100. - ((double)rand() / RAND_MAX) * 200.;
+        }
+        // заповнив a[i] та b[i] випадковими числами від -100 до 100
+        int R1 = 0, R2 = 0;
+        for (int i = 1; i <= 15; i++)
+        {
+            R1 = R1 + (a[2 * i - 1] + b[2 * i - 1]);
+            R2 = R2 + (a[2 * i] + b[2 * i]);
+        }
+        int Rez;
+        Rez = R1 / R2;
+        cout << Rez << endl;
+        
+    }
+
---
+++ lab04/lab0402/lab0402.cpp
@@ -0,0 +1,29 @@
+﻿// 10
+
+#include <iostream>
+using namespace std;
+main()
+{
+    int a[100], b[100];
+    int i, max;
+    for (i = 0; i < 100; i++)
+        ;
+    max = a[0];
+    for (i = 0; i < 100; i++)
+    {
+        if (a[i] > max)
+            max = a[i];
+    }
+    for (i = 0; i < 100; i++)
+    {
+        if (a[i] != max)
+            b[i] = 0;
+        else
+            b[i] = 1;
+        cout << b[i];
+    }
+    return 0;
+}
+
+
+
