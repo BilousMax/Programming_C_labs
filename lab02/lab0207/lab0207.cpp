9c354c52e6d2f3aab7ab13a748d92f2837c49527
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-05-01 16:03:42 +0300

    lab0207

---
+++ lab02/lab0207/lab0207.cpp
@@ -0,0 +1,22 @@
+﻿// 11
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int n, a, sum = 0;
+	cout << "n= ";
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+
+		cout << " a = ";
+		cin >> a;
+	}
+	for (int j = 1; j <= n; j++) {
+		sum = sum + (a/j);
+	}
+	cout << sum;
+}
+
+
