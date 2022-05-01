0d6c772c17f3f974a6e87e50e7093579ebaba5d4
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-05-01 16:22:59 +0300

    lab0203

---
+++ lab02/lab0203/lab0203.cpp
@@ -0,0 +1,20 @@
+﻿// 3
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    
+    float f, a = 0;
+    cout << "ENTER a: ";
+    cin >> a;
+    if (a < -1)
+        f = 0;
+    else
+        if (a < 1)
+            f = sqrt(1 - pow(a, 2));
+        else 
+            f = a - 1;
+    cout << "RESULT\n f= " << f;
+}
+
