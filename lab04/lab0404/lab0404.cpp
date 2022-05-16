2cf2c76cd6914360a95dfd9eb3474504481e0961
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-05-16 11:04:48 +0300

    lab0404

--- lab04/lab0404/lab0404.cpp
+++ lab04/lab0404/lab0404.cpp
@@ -1,39 +1,40 @@
 ﻿// 9
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
 int main()
 {
     setlocale(LC_ALL, "ukr");
-    int n,i,j,a[10][10];
+
+    int a[100][100], i, j, m, n;
+    cout << "Введiть кiлькiсть рядкiв i стовпцiв (матриця має бути квадратною):" << endl;
+    cout << "Кiлькiсть рядкiв = ";
+    cin >> m;
+    cout << "Кiлькiсть стовпцiв = ";
     cin >> n;
     srand((unsigned)time(NULL));
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < m; i++) {
         for (j = 0; j < n; j++) {
-            a[i][j] = 300 * rand() / RAND_MAX;
-
-
-            cout << " \t" << a[i][j];
+            a[i][j] = 100 * rand() / RAND_MAX;
         }
 
     }
-    int max = a[0][0];
-    for (i = 0; i < n; i++)
+    cout << "Матриця : " << endl;
+    for (i = 0; i < m; i++)
+    {
         for (j = 0; j < n; j++)
-            if (a[i][j] > a[0][0])
-                max = a[i][j];
-            else
-                max != a[i][j];
-
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n; j++)
-            if (max = n + 1)
-                max = a[i][j];
-            else
-                max != a[i][j];
-    cout << "\n\t";
-    cout << " \t" << max;
-    
+            cout << setw(5) << a[i][j];
+        cout << endl;
+    }
+    int max_i = 0, max_j = 0;
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            if (a[i][j] > a[max_i][max_j])
+            {
+                max_i = i; max_j = j;
+            }
+    cout << "Максимальний елемент бiчної дiагоналi\n";
+    cout << a[max_i][max_j];
 }
-
-
