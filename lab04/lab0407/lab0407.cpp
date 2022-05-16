fef85e39e7da7191e005852ebcc191ffeb2cb974
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-05-16 08:59:48 +0300

    lab0407

---
+++ lab04/lab0407/lab0407.cpp
@@ -0,0 +1,39 @@
+﻿// 7
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ukr");
+
+    int a[25][25], i, j, m, n;
+    cout << "Введiть кiлькiсть рядкiв i стовпцiв :" << endl;
+    cout << "Кiлькiсть рядкiв = ";
+    cin >> m;
+    cout << "Кiлькiсть стовпцiв = ";
+    cin >> n;
+    srand((unsigned)time(NULL));
+    for (i = 0; i < 25; i++) {
+        for (j = 0; j < 25; j++) {
+            a[i][j] = 300 * rand() / RAND_MAX;
+        }
+
+    }
+    cout << "Матриця : " << endl;
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+            cout << setw(5) << a[i][j];
+        cout << endl;
+    }
+    cout << "Транспонована матриця : " << endl;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+            cout << setw(5) << a[j][i];
+        cout << endl;
+    }
+}
+
