18c1fc584b4d2c296e105e47402db7854128d0ae
BilousMax <kn1b21.bilous@kpnu.edu.ua>
2022-05-25 08:28:22 +0300

    .

---
+++ lab05/lab0502/lab0502.cpp
@@ -0,0 +1,24 @@
+﻿// 5
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    setlocale (LC_CTYPE, "ukr");
+    auto anagrama = [](string& a,string& b) {
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        return includes(a.begin(), a.end(), b.begin(), b.end());
+    };
+    string s1, s2;
+    cout << "Введiть перше слово\n";
+    cin >> s1;
+    cout << "Введiть друге слово\n";
+    cin >> s2;
+    cout << boolalpha << anagrama(s1, s2);
+   
+}
+
+
---
+++ lab05/lab0503/lab0503.cpp
@@ -0,0 +1,35 @@
+﻿// 2
+
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    using namespace std;
+    char str[50];
+    int i, n_a, n_o;
+    setlocale(LC_CTYPE, "ukr");
+    cout << "Текст : ";
+    cin >> str;
+    n_a = 0;
+    n_o = 0;
+    for (i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == 'а')
+            n_a++;
+        if (str[i] == 'о')
+            n_o++;
+        
+    }
+    if (n_a > n_o)
+        cout << "Бiльше символiв А\n";
+    else if (n_o > n_a)
+        cout << "Бiльше символiв О\n";
+    if (n_a == 0)
+    cout << "Символ А не найдено\n";
+    if (n_o == 0)
+        cout << "Символ О не найдено\n";
+    cout << "A-" << n_a << endl;
+    cout << "O-" << n_o;
+}
+
